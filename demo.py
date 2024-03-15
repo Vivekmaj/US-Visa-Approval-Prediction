597926a0348cd996ea5c4fdc67cb511110c3e825
Vivekmaj <vivekmaj96@gmail.com>
2024-03-15 18:43:23 +0000

    Exception Added

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
 
-logging.info("Hello World")
+try:
+    a = 1/0
+except Exception as e:
+    raise USvisaException(e, sys)
