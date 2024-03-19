53acc63d7313b4fe109721f7fd875796715774c0
Vivekmaj <vivekmaj96@gmail.com>
2024-03-19 17:51:03 +0000

    Data Ingestion

--- demo.py
+++ demo.py
@@ -1,9 +1,5 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipeline.training_pipeline import TrainPipeline
 
-try:
-    a = 1/0
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys)
+
+pipeline  = TrainPipeline()
+pipeline.run_pipeline()
